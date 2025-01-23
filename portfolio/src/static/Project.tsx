9716ea8d0ff84d76f5bb3a60411ee5216328551e
krishnakashiv <krishna.kashiv@gmail.com>
2025-01-23 13:54:36 -0500

    Fixed links, open new tabs for project

--- portfolio/src/static/Project.tsx
+++ portfolio/src/static/Project.tsx
@@ -1,7 +1,7 @@
 import Tag from "./Tag";
 
 function Project ({ headline, image, text, tags, link }: { headline: string, image: string, text: string, tags: string[], link: string }) {
-    return (<a href={link} aria-label={text}>
+    return (<a href={link} target="_blank" rel="noopener noreferrer" aria-label={text}>
     <div className="flex flex-col transition-all hover:scale-105 scale-100 bg-opacity-20 bg-black rounded-lg max-h-max max-w-96 m-3 text-white">
         <img className="w-full max-w-md aspect-w-16 aspect-h-9 rounded-t-lg overflow-hidden" alt={text} src={image}/>
         <div className="p-4">
